faf77a3575f3d9ca47d81957d6a8ed7cde9175ba
Lord <volin232c@gmail.com>
2025-02-04 14:49:59 +0200

    ctg function

---
+++ Lab1/lab1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+double ctg(double, double);
+
+int main(int argc, char const *argv[])
+{
+    double result;
+    
+
+
+    return 0;
+}
+
+double ctg(double a, double b){
+    double result;
+    result = cos(a) / sin(b);
+    return result;
+}
